6b9a52e7f6de2dc69ddf6b9fb81d6a2e693e3dd0
PeaceSamuel03 <136840451+PeaceSamuel03@users.noreply.github.com>
2024-02-09 19:21:29 +0000

    Add files via upload
    A simple FLASK app that displays the first fashion item from the data file, in json format -> which is the format need to integrate with Flutter

---
+++ test_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+asos_df = pd.read_csv('data.csv')
+@app.route('/')
+def index():
+    """DISPLAY IN THE WEBPAGE THE FIRST ITEM THAT IS IN THE DATA FILE"""
+    name_ = asos_df['Name'][0]
+    brand_ = asos_df['Brand'][0]
+    price_ = asos_df['Price'][0]
+    image = "https://" + asos_df['Image_URL'][0]
+    product_link = "https://asos.com/"+asos_df['Product_URL'][0]
+    first_product = {
+        'name': name_,
+        'brand': brand_,
+        'price': price_,
+        'image': image,
+        'link': product_link,
+    }
+    return jsonify(first_product)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
