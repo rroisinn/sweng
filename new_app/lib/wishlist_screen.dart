dca80f24c5da867cd0601f75c2d2a18851d09368
Yasmin Murphy <121475002@umail.ucc.ie>
2024-02-19 15:05:00 +0000

    added blank wishlist, button.

---
+++ new_app/lib/wishlist_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wishlist'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Center(
+        child: Text('Your wishlist is currently empty.'),
+      ),
+    );
+  }
+}
